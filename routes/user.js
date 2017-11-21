6194b57794604ca34e89380a60dc543b5eced35d
Kiet Pham <phamquanganhkiet@gmail.com>
2017-11-21 21:18:41 +1100

    signup form template

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 var router = require('express').Router();
 var User = require('../models/user');
 
+router.get('/signup', function(req, res) {
+    res.render('accounts/signup');
+});
+
 router.post('/signup', function(req, res, next) {
     var user = new User();
 

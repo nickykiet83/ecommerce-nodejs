cee1b757fb3f5dadb71cc1a438dbbd76ee10d055
Kiet Pham <phamquanganhkiet@gmail.com>
2017-11-21 19:46:54 +1100

    seperating routes

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var engine = require('ejs-mate');
 
 var app = express();
 
-var User = require('./models/user');
-
 mongoose.connect('mongodb://root:123456@ds113906.mlab.com:13906/ecommerce', function(err) {
     if (err) {
         console.log(err);
@@ -26,27 +24,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
-app.post('/create-user', function(req, res, next) {
-    var user = new User();
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
 
-    user.profile.name = req.body.name;
-    user.email = req.body.email;
-    user.password = req.body.password;
+app.use(mainRoutes);
+app.use(userRoutes);
 
-    user.save(function(err) {
-        if (err) return next(err);
-
-        res.json('Successfully created a new user');
-    });
-});
-
-app.get('/', function (req, res){
-    res.render('main/home');
-});
-
-app.get('/about', function(req, res) {
-    res.render('main/about');
-});
 
 app.listen(port, function(err) {
     if (err) throw err;

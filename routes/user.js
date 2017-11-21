b7fc9400f0e7b38484b28300bcfb6b39bbeaf8fa
Kiet Pham <phamquanganhkiet@gmail.com>
2017-11-21 23:52:22 +1100

    Login and authenticate with passport

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,35 @@
 var router = require('express').Router();
 var User = require('../models/user');
+var passport = require('passport');
+var passportConf = require('../config/passport');
 
-router.get('/signup', function(req, res) {
+router.get('/login', function (req, res) {
+    if (req.user) return res.redirect('/');
+
+    res.render('accounts/login', { message: req.flash('loginMessage') });
+});
+
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+router.get('/profile', function(req, res, next) {
+    User.findOne({ _id: req.user._id }, function(err, user) {
+        if (err) return next(err);
+
+        res.render('accounts/profile', { user: user });    
+    });
+});
+
+router.get('/signup', function (req, res) {
     res.render('accounts/signup', {
         errors: req.flash('errors')
     });
 });
 
-router.post('/signup', function(req, res, next) {
+router.post('/signup', function (req, res, next) {
     var user = new User();
 
     user.profile.name = req.body.name;
@@ -15,14 +37,14 @@ router.post('/signup', function(req, res, next) {
     user.password = req.body.password;
     console.log(req.body);
 
-    User.findOne({ email: req.body.email }, function(err, existingUser) {
+    User.findOne({ email: req.body.email }, function (err, existingUser) {
         if (existingUser) {
             req.flash('errors', 'Account with that email address already exists');
             return res.redirect('/signup');
         } else {
-            user.save(function(err) {
+            user.save(function (err) {
                 if (err) return next(err);
-        
+
                 return res.redirect('/');
             });
         }
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ var engine = require('ejs-mate');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var flash = require('express-flash');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
 
 var secret = require('./config/secret');
 
 var app = express();
 
-mongoose.connect(secret.database, function(err) {
+mongoose.connect(secret.database, function (err) {
     if (err) {
         console.log(err);
     } else {
@@ -31,18 +33,25 @@ app.use(cookieParser());
 app.use(session({
     resave: true,
     saveUninitialized: true,
-    secret: secret.secretKey
+    secret: secret.secretKey,
+    store: new MongoStore({
+        url: secret.database,
+        autoReconnect: true,
+    })
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
+//register routes
 var mainRoutes = require('./routes/main');
 var userRoutes = require('./routes/user');
 
 app.use(mainRoutes);
 app.use(userRoutes);
 
-
-app.listen(secret.port, function(err) {
+//start server
+app.listen(secret.port, function (err) {
     if (err) throw err;
     console.log("Server is Running at port: " + secret.port);
 });

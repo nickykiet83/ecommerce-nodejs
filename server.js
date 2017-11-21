3c016171ec46dec107b9c0222f5f7c6e8424df87
Kiet Pham <phamquanganhkiet@gmail.com>
2017-11-21 18:41:38 +1100

    Create a route for testing UserSchema

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 const port = 3000;
 var app = express();
+
+var User = require('./models/user');
 
 mongoose.connect('mongodb://root:123456@ds113906.mlab.com:13906/ecommerce', function(err) {
     if (err) {
@@ -14,14 +17,22 @@ mongoose.connect('mongodb://root:123456@ds113906.mlab.com:13906/ecommerce', func
 
 //Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-    let name = "Kiet Pham";
-    res.json("My name is: " + name);
-});
 
-app.get('/catname', function(req, res) {
-    res.json('batman');
+app.post('/create-user', function(req, res, next) {
+    var user = new User();
+
+    user.profile.name = req.body.name;
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    user.save(function(err) {
+        if (err) next(err);
+
+        res.json('Successfully created a new user');
+    });
 });
 
 app.listen(port, function(err) {
